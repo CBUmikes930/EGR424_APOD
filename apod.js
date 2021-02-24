bfb20174e34e095d15a57364549118028e0cba5c
CBUmikes930 <Micahwilliam.steinbock@calbaptist.edu>
2021-02-23 20:37:34 -0800

    Added xhttp ajax approach

--- apod.js
+++ apod.js
@@ -87,6 +87,17 @@ const displayError = error => {
     $("#display").html(html);
 };
 
+function displayTestData() {
+    console.log("Test");
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("display").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "https://jsonplaceholder.typicode.com/users", true);
+    xhttp.send();
+};
 
 $(document).ready( () => {
     const today = new Date();
